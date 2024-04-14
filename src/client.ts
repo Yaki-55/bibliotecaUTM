936364844634ee288c8103d0c6c9cc92389415d2
Yaki55 <mely020202@gs.utm.mx>
2024-04-13 21:17:45 -0600

    conexion con la base de datos

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client'
 
 export default createClient({
-    projectId: "8kdpxld9",
+    projectId: "5rc1pjr5",
     dataset: "production",
     useCdn: true,
     apiVersion: '2021-03-25',
