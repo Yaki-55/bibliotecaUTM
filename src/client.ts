c4fc533b970ddee2c38ae2c8ca13b17042981925
FitoGod <marvinsalva1236@gmail.com>
2024-04-20 21:37:28 -0600

    Quitar archivos innecesarios
    Se removio archivos innecesarios como src y otras carpetas.

--- src/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client'
-
-export default createClient({
-    projectId: "5rc1pjr5",
-    dataset: "production",
-    useCdn: true,
-    apiVersion: '2021-03-25',
-})
