8f2517e86a4840372a6822caffb337e7f6093934
blanca000 <blanca12024@gmail.com>
2024-04-29 06:26:01 -0600

    actualización

--- cliente_biblioteca/src/interface/libro.tsx
+++
@@ -1,7 +0,0 @@
-interface Libro {
-  id: number; // Unique identifier for the book
-  titulo: string; // Title of the book
-  autor: string; // Author of the book
-  fechaPublicacion: string; // Publication date of the book
-  genero: string; // Genre of the book
-}
