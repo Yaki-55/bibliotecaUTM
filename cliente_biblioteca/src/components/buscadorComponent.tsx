e54e3154c6dc5fa725b8f9d8218531d58c1946fa
blanca000 <blanca12024@gmail.com>
2024-04-28 21:43:36 -0600

    Actualización buscador

--- cliente_biblioteca/src/components/buscadorComponent.tsx
+++ cliente_biblioteca/src/components/buscadorComponent.tsx
@@ -1,187 +1,142 @@
-import React, { useEffect, useState } from "react";
-import client from "../utils/sanityClient";
-import "../css/buscador.css";
+import React, { useEffect, useState } from 'react';
+import client from '../utils/sanityClient';
+import '../css/buscador.css';
+
+
 
 const BuscadorComponent: React.FC = () => {
-  const [libros, setLibros] = useState<any[]>([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchType, setSearchType] = useState("titulo");
-  const [documentType, setDocumentType] = useState(""); // Nuevo estado para el tipo de documento
-  const [mostrarResultados, setMostrarResultados] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [selectedLibro, setSelectedLibro] = useState(null); // Add state variable
+    
+
+    const [libros, setLibros] = useState<any[]>([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchType, setSearchType] = useState('titulo');
+    const [documentType, setDocumentType] = useState(''); // Nuevo estado para el tipo de documento
+    const [mostrarResultados, setMostrarResultados] = useState(false);
+    const [libroSeleccionado, setLibroSeleccionado] = useState<any | null>(null); // Estado para almacenar el libro seleccionado
 
 
-  // const openModal = () => {
-  //   setModalVisible(true);
-  // };
-
-  const openModal = (libroSeleccionado) => { // Add parameter 'libroSeleccionado'
-    setModalVisible(true);
-    setSelectedLibro(libroSeleccionado); // Store the selected book
-  };
-  
-
-  const closeModal = () => {
-    setModalVisible(false);
-  };
-
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const query = '*[_type == "libro"]';
-      const libros = await client.fetch(query);
-      console.log('Libros recuperados:', libros);
-      setLibros(libros);
+    const openModal = () => {
+        setModalVisible(true);
     };
-    fetchBooks();
-  }, []);
+    const closeModal = () => {
+        setModalVisible(false);
+    };
 
+    
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
-  const fetchBooks = async () => {
-    const query = '*[_type == "libro"]';
-    const fetchedLibros = await client.fetch(query);
-    console.log("Libros recuperados:", fetchedLibros);
-    setLibros(fetchedLibros);
-  };
+    const fetchBooks = async () => {
+        const query = '*[_type == "libro"]';
+        const fetchedLibros = await client.fetch(query);
+        console.log('Libros recuperados:', fetchedLibros);
+        setLibros(fetchedLibros);
+    };
 
-  const handleSearchTermChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchTerm(event.target.value);
-  };
+    const handleSearchTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
 
-  const handleSearchTypeChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchType(event.target.value);
-  };
+    const handleSearchTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchType(event.target.value);
+    };
 
-  const handleDocumentTypeChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setDocumentType(event.target.value);
-  };
+    const handleDocumentTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDocumentType(event.target.value);
+    };
 
-  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    const handleViewDetails = (libro: any) => {
+        setLibroSeleccionado(libro); // Actualiza el estado con el libro seleccionado
+        openModal(); // Abre el modal
+    };
 
-    let typesToSearch = documentType
-      ? [documentType]
-      : ["libro", "revista", "tesis"];
-    let query = '';
-
-    if (searchTerm) {
-      // Ajusta la consulta dependiendo de si "Todo" ha sido seleccionado o un campo específico
-      if (searchType === '') {
-          // "Todo" está seleccionado
-          query = `*[
-              _type in ${JSON.stringify(typesToSearch)} && (
-                  titulo match "${searchTerm}*" ||
-                  autor match "${searchTerm}*" ||
-                  tema match "${searchTerm}*"
-              )
-          ]`;
-      } else {
-          // Un campo específico ha sido seleccionado
-          query = `*[
-              _type in ${JSON.stringify(typesToSearch)} && ${searchType} match "${searchTerm}*"
-          ]`;
-      }
-  } else {
-      // Si no hay término de búsqueda, recopila todos los elementos de todos los tipos
-      query = `*[_type in ${JSON.stringify(typesToSearch)}]`;
-  }
-
-    try {
-      const searchResults = await client.fetch(query);
-      console.log(searchResults);
-      // Actualiza el estado aquí con los resultados si es necesario
-      setLibros(searchResults); // Actualiza el estado con los resultados
-      setMostrarResultados(true); //Activa para poder mostrar los resultados
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setMostrarResultados(false);
-    }
-
-    // Limpia el término de búsqueda después de la búsqueda
-    setSearchTerm("");
-  };
-
-  const renderResultsTable = () => (
-    <table className="results-table">
-      <thead>
-        <tr>
+    const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        let typesToSearch = documentType ? [documentType] : ['libro', 'revista', 'tesis'];
+        let query = '';
+    
+        if (searchTerm) {
+            // Ajusta la consulta dependiendo de si "Todo" ha sido seleccionado o un campo específico
+            if (searchType === '') {
+                // "Todo" está seleccionado
+                query = `*[
+                    _type in ${JSON.stringify(typesToSearch)} && (
+                        titulo match "${searchTerm}*" ||
+                        autor match "${searchTerm}*" ||
+                        tema match "${searchTerm}*"
+                    )
+                ]`;
+            } else {
+                // Un campo específico ha sido seleccionado
+                query = `*[
+                    _type in ${JSON.stringify(typesToSearch)} && ${searchType} match "${searchTerm}*"
+                ]`;
+            }
+        } else {
+            // Si no hay término de búsqueda, recopila todos los elementos de todos los tipos
+            query = `*[_type in ${JSON.stringify(typesToSearch)}]`;
+        }
+        
+        try {
+            const searchResults = await client.fetch(query);
+            console.log(searchResults);
+            // Actualiza el estado aquí con los resultados si es necesario
+            setLibros(searchResults); // Actualiza el estado con los resultados
+            setMostrarResultados(true);  //Activa para poder mostrar los resultados
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            setMostrarResultados(false); 
+        }
+        
+        // Limpia el término de búsqueda después de la búsqueda
+        setSearchTerm('');
+    };
+    
+    const renderResultsTable = () => (
+        <table className="results-table">
+            <thead>
+            <tr>
                 <th className='hmno'>Autor</th>
                 <th className='hmno'>Título</th>
                 <th className='hmno'>Código</th>
                 <th className='hmno'>Más</th>
-        </tr>
-      </thead>
-      <tbody>
-        {libros.map((libro, index) => (
-          <tr key={index}>
+            </tr>
+            </thead>
+            <tbody>
+            {libros.map((libro, index) => (
+                
+                <tr key={index}>
                     <td className='hmno'>{libro.autor}</td>
                     <td className='hmno'>{libro.titulo}</td>
                     <td className='hmno'>{libro.codigo}</td>
-            <td>
-              {/* Aquí iría el botón o enlace para ver más detalles */}
-              {/* <button onClick={openModal}>Ver más</button> */}
-              <button onClick={() => openModal(libro)}>Ver más</button>
-              {modalVisible && (
-                <div className="modal">
-                  <div className="modal-content">
-                    {/* Formulario para mostra un libro/tesis/revista especifico */}
-                    <h2 className='hmno'>Detalles</h2>
-                    <div>
-                      <div>
-                        <label id="label1">Autor: </label>
-                        <label id="label2">{selectedLibro.autor}</label>                        
-                      </div>
-                      <div>
-                        <label id="label1">Título: </label>
-                        <label id="label2">{selectedLibro.titulo}</label>                         
-                      </div>
-                      <div>
-                        <label id="label1" >Fecha de Publicación: </label>
-                        <label id="label2">{selectedLibro.fechaPublicacion}</label>                        
-                      </div>
-                      <div>
-                        <label id="label1">Edición: </label>
-                        <label id="label2">{selectedLibro.edicion}</label>                         
-                      </div>
-                    </div>
-                    <button onClick={closeModal}>Cerrar</button>
-                  </div>
-                </div>
+                    <td>
+                  {/* Aquí iría el botón o enlace para ver más detalles */}
+                    <button onClick={() => handleViewDetails(libro)} >Ver más</button>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
 
-              )}
-
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-
-  return (
+    return (
     <div className="buscador-container">
       <div className="home-logo">
         <img
           src="../../public/img/logoUTM.png"
           alt=""
-          width="200"
-          height="200"
+          width="150"
+          height="150"
         />
         <h1>Hemeroteca/Biblioteca</h1>
       </div>
       <br></br>
-
-      <form onSubmit={handleSearch}>
+        <form onSubmit={handleSearch}>
         {/* Filtro por Tipo */}
+
         <div className="inputBuscar-personalizado">
           <div className="imagen-buscar">
             <img
@@ -201,55 +156,54 @@ const BuscadorComponent: React.FC = () => {
             />
           </div>
         </div>
-
-        <div className="buscador-container-tipos"> 
-          <label>
-            <h4> Tipo:   </h4>
+        <div className='hmno'>
+            Tipo:
+            <label>
             <input
-              type="radio"
-              name="documentType"
-              value="libro"
-              checked={documentType === "libro"}
-              onChange={handleDocumentTypeChange}
+                type="radio"
+                name="documentType"
+                value="libro"
+                checked={documentType === 'libro'}
+                onChange={handleDocumentTypeChange}
             />
-            <h4>Libro</h4>
-          </label>
-          <label>
+            Libro
+            </label>
+            <label>
             <input
-              type="radio"
-              name="documentType"
-              value="revista"
-              checked={documentType === "revista"}
-              onChange={handleDocumentTypeChange}
+                type="radio"
+                name="documentType"
+                value="revista"
+                checked={documentType === 'revista'}
+                onChange={handleDocumentTypeChange}
             />
-            <h4>Revista</h4>
-          </label>
-          <label>
+            Revista
+            </label>
+            <label>
             <input
-              type="radio"
-              name="documentType"
-              value="tesis"
-              checked={documentType === "tesis"}
-              onChange={handleDocumentTypeChange}
+                type="radio"
+                name="documentType"
+                value="tesis"
+                checked={documentType === 'tesis'}
+                onChange={handleDocumentTypeChange}
             />
-            <h4>Tesis</h4>
-          </label>
-          <label>
+            Tesis
+            </label>
+        <label>
             <input
-              type="radio"
-              name="documentType"
-              value="" // Representa la opción "Todos"
-              checked={documentType === ""}
-              onChange={handleDocumentTypeChange}
+                type="radio"
+                name="documentType"
+                value=""  // Representa la opción "Todos"
+                checked={documentType === ''}
+                onChange={handleDocumentTypeChange}
             />
-            <h4>Todos</h4>
-          </label>
+            Todos
+        </label>
         </div>
-
+        
         {/* Filtro por Título, Autor, Tema */}
-
-                
-        <div>
+        
+        
+        <div className='hmno'>
             Buscar por:
             <label>
             <input
@@ -322,18 +276,73 @@ const BuscadorComponent: React.FC = () => {
             Todo
             </label>
         </div>
+        <button type="submit" className="search-button">Buscar</button>
+        </form>
+        {/* Tabla de libros aquí */}
+        {/* Renderiza la tabla de resultados si hay resultados para mostrar */}
+        {mostrarResultados && renderResultsTable()}
+        <a href="/" className="salir-libro">Salir</a> 
 
-        <button type="submit" className="search-button">
-          Buscar
-        </button>
-      </form>
-      {/* Tabla de libros aquí */}
-      {/* Renderiza la tabla de resultados si hay resultados para mostrar */}
-      {mostrarResultados && renderResultsTable()}
-      <a href="/" className="salir-libro">
-        SALIR
-      </a>
+
+        {/* Modal */}
+      {modalVisible && (
+        <div className="modal" >
+            {/* Formulario para agregar un nuevo libro */}
+              
+              <div className="formularioRevista" id="formularioRevista"> 
+
+
+                <div className="formulario_revista" id="revista_fechaPublicacion">
+                  <label className="formulario__label">Edición: </label>
+                  <div className="formulario_revista-input">
+                    <input type="number" className="formulario__input" value={libroSeleccionado?.edicion || ''} />
+                  </div>
+                </div>
+
+      
+                  <div className="formulario_revista" id="revista_fecha">
+                    <label className="formulario__label">Fecha de publicación: </label>
+                    <div className="formulario_revista-input">
+                      <input type="date" className="formulario__input" value={libroSeleccionado?.fechaPublicacion || ''}  />
+                    </div>
+                  </div>
+
+
+
+                  <div className="formulario_revista" id="revista_Tema">
+                    <label  className="formulario__label">Tema: </label>
+                    <div className="formulario_revista-input">
+                      <input type="text" className="formulario__input" value={libroSeleccionado?.tema|| ''} />
+                    </div>
+                  </div>
+
+                  <div className="formulario_revista" id="revista_titulo">
+                    <label  className="formulario__label">Titulo: </label>
+                    <div className="formulario_revista-input">
+                      <input type="text" className="formulario__input" value={libroSeleccionado?.titulo || ''} />
+                    </div>
+                  </div>
+
+                  <div className="formulario_revista" id="revista_autor">
+                    <label  className="formulario__label">Autor: </label>
+                    <div className="formulario_revista-input">
+                      <input type="text" className="formulario__input"   value={libroSeleccionado?.autor || ''}/>
+                    </div>
+                  </div>
+
+      
+                  <div  className="botones">
+                    <button type="submit" className="formulario__btn1" onClick={closeModal} >Cancelar</button>
+                    <button type="submit" className="formulario__btn2"  >Reservar</button>
+                  </div>
+
+            </div>
+        </div>
+        
+      )}
+
     </div>
+    
   );
 };
 
---
+++ cliente_biblioteca/src/interface/libro.tsx
@@ -0,0 +1,7 @@
+interface Libro {
+  id: number; // Unique identifier for the book
+  titulo: string; // Title of the book
+  autor: string; // Author of the book
+  fechaPublicacion: string; // Publication date of the book
+  genero: string; // Genre of the book
+}
